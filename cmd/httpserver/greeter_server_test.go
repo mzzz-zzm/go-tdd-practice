bbebe064f6eb9c4295176598d142812c151f4abb
mzzz-zzm <mzzz-zzm@example.com>
2025-01-26 00:15:47 +0000

    clean up code

--- cmd/httpserver/greeter_server_test.go
+++ cmd/httpserver/greeter_server_test.go
@@ -2,7 +2,6 @@ package main_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
@@ -12,7 +11,8 @@ import (
 	"github.com/mzzz-zzm/go-tdd-practice/specifications"
 )
 
-// go test -v -timeout 300s -run ^TestGreeterServer$ github.com/mzzz-zzm/go-tdd-practice/cmd/httpserver
+// first time go mod download takes a long time, avoid timeout by increasing -timeout
+// ex) go test -v -timeout 300s -run ^TestGreeterServer$ github.com/mzzz-zzm/go-tdd-practice/cmd/httpserver
 func TestGreeterServer(t *testing.T) {
 	compose, err := tc.NewDockerCompose("../../.devcontainer/docker-compose.yml")
 	assert.NoError(t, err)
@@ -26,46 +26,21 @@ func TestGreeterServer(t *testing.T) {
 	err = compose.Up(ctx, tc.Wait(true))
 	assert.NoError(t, err)
 
-	// ctx := context.Background()
-
-	// req := testcontainers.ContainerRequest{
-	// 	FromDockerfile: testcontainers.FromDockerfile{
-	// 		Context:    "../../.",
-	// 		Dockerfile: "./Dockerfile",
-	// 		// set to false if you want less spam, but this is helpful if you're having troubles
-	// 		PrintBuildLog: true,
-	// 	},
-	// 	ExposedPorts: []string{"8080:8080"},
-	// 	WaitingFor:   wait.ForHTTP("/").WithPort("8080"),
-	// }
-	// container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-	// 	ContainerRequest: req,
-	// 	Started:          true,
-	// })
-	// assert.NoError(t, err)
-	// t.Cleanup(func() {
-	// 	assert.NoError(t, container.Terminate(ctx))
-	// })
-
-	// ip, err := container.Host(ctx)
-	// assert.NoError(t, err)
-	// mappedPort, err := container.MappedPort(ctx, "8080")
-	// assert.NoError(t, err)
-	// containerUrl := fmt.Sprintf("http://%s:%s", ip, mappedPort)
-
-	// containerUrl := "http://testsvr:8080"
 	testsvrContainer, err := compose.ServiceContainer(ctx, "testsvr")
 	assert.NoError(t, err)
 
-	port, err := testsvrContainer.MappedPort(ctx, "8080/tcp")
-	assert.NoError(t, err)
-	host, err := testsvrContainer.Host(ctx)
-	assert.NoError(t, err)
-	url := fmt.Sprintf("http://%s:%s", host, port.Port())
-
-	// endPt, err := testsvrContainer.Endpoint(ctx, "http")
+	// --- case 1: use MappedPort and Host ---
+	// port, err := testsvrContainer.MappedPort(ctx, "8080/tcp")
 	// assert.NoError(t, err)
+	// host, err := testsvrContainer.Host(ctx)
+	// assert.NoError(t, err)
+	// url := fmt.Sprintf("http://%s:%s", host, port.Port())
+	// driver := go_specs_greet.Driver{BaseURL: url}
 
-	driver := go_specs_greet.Driver{BaseURL: url}
+	// --- case 2: use Endpoint ---
+	endPt, err := testsvrContainer.Endpoint(ctx, "http")
+	assert.NoError(t, err)
+	driver := go_specs_greet.Driver{BaseURL: endPt}
+
 	specifications.GreetSpecifications(t, driver)
 }
--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	handler := http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello, world"))
 	})
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
